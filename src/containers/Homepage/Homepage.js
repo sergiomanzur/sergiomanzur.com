f55669f93f7a7c03e34670a65094791c53d57d5b
Sergio Manzur Tapia <sergio@mitunetwork.com>
2019-10-11 17:54:25 -0500

    - added adsense to the template - Converted Homepage and Post components to classes

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 //import classes from './NoMatch.module.css';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -9,54 +9,67 @@ import axios from 'axios';
 import ReactGA from "react-ga";
 
 
-const Homepage = () => {
+class Homepage extends Component {
 
-    ReactGA.initialize('UA-65003463-1');
-    ReactGA.pageview('/');
 
-    const [posts,setPost] = useState([]);
-    const[currentPage, setCurrentPage] = useState(null);
-    const[totalPages, setTotalPages] = useState(null);
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+            currentPage : null,
+            totalPages: null
+        };
+    }
 
-    useEffect(() => {
-        getInformation(1);
-    }, []);
+    componentDidMount() {
+        this.getInformation(1);
+        (window.adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-1787291928862475",
+            enable_page_level_ads: true
+        });
+    }
 
-        const getInformation = (page) => {
-            axios.get(`http://www.dev.sergiomanzur.com/api/pages/page${page}`)
-                .then(res => {
-                    setPost(res.data.data);
-                    setCurrentPage(res.data['current_page']);
-                    setTotalPages(res.data['total_pages']);
-                })
-        }
+    getInformation(page) {
+        axios.get(`http://www.dev.sergiomanzur.com/api/pages/page${page}`)
+            .then(res => {
+                this.setState({posts:res.data.data});
+                this.setState({currentPage: res.data['current_page']});
+                this.setState({totalPages:res.data['total_pages']});
+            })
+    }
+
+     handlePageChange (page) {
+        this.getInformation(page);
+    }
+
+    render() {
+
+        ReactGA.initialize('UA-65003463-1');
+        ReactGA.pageview('/');
 
         const handlePageChange = (page) => {
-            getInformation(page);
+            this.getInformation(page);
         }
 
+        const categories = this.state.categories;
+        const card = <HomepageCard/>;
+        const homePosts = this.state.posts.filter((x,i)=> i < 6 ).map(
+            x => <HomepageCard key={x.id} title={x.title}
+                               image={"http://sergiomanzur.com/site/assets/files/"+x.id+"/"+
+                               x.images[0].data} summary={x.summary} slug={x.slug} author={x.author} fecha={x.fecha}/>);
 
-
-
-    const homePosts = posts.filter((x,i)=> i < 6 ).map(
-        x => <HomepageCard key={x.id} title={x.title}
-                           image={"http://sergiomanzur.com/site/assets/files/"+x.id+"/"+
-                           x.images[0].data} summary={x.summary} slug={x.slug} author={x.author} fecha={x.fecha}/>);
-    //console.log(titles);
-
-    const  card = <HomepageCard/>;
-
-    return (
-        <Container>
-            <br/>
-            <Row>
-                <Col sm={12}>
-                    <Row>{homePosts}</Row>
-                </Col>
-            </Row>
-            {currentPage ? <Pagination handlePageChange={handlePageChange} totalPages={totalPages} current={currentPage}/> :  null}
-        </Container>
-    )
+        return(
+            <Container>
+                <br/>
+                <Row>
+                    <Col sm={12}>
+                        <Row>{homePosts}</Row>
+                    </Col>
+                </Row>
+                {this.state.currentPage ? <Pagination handlePageChange={handlePageChange} totalPages={this.state.totalPages} current={this.state.currentPage}/> :  null}
+            </Container>
+        );
+    }
 }
 
 export default Homepage;
--- src/containers/Post/Post.js
+++ src/containers/Post/Post.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import classes from './Post.module.css';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -36,193 +36,210 @@ import {
     RedditIcon,
     EmailIcon,
 } from 'react-share';
+import AdSense from "react-adsense";
 
 
-
-const Post = ({match}) => {
-
-    ReactGA.initialize('UA-65003463-1');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-
-    const [posts, setPost] = useState([]);
-    const slug = match.params.slug;
+class Post extends Component {
 
 
-    useEffect(() => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+            currentPage : null,
+            totalPages: null,
+            slug: props.match.params.slug
+        };
+    }
 
-        axios.get('http://www.dev.sergiomanzur.com/api/pages/' + slug)
+    componentDidMount() {
+        (window.adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-1787291928862475",
+            enable_page_level_ads: true
+        });
+
+        axios.get('http://www.dev.sergiomanzur.com/api/pages/' + this.state.slug)
             .then(res => {
-                setPost(res.data);
+                this.setState({posts:res.data})
             }, (error) => {
                 console.log(error);
-                setPost(false);
+                this.setState({posts: false});
             })
-    }, []);
+    }
 
 
-    if (posts === false) {
 
-        return (<NoMatch/>);
+    render() {
 
-    } else {
-        const title = posts.title;
-        const content = [posts.body];
-        const first_image = <img style={{width: "100%", marginBottom: "25px"}} src={posts.main_image}/>
-        const summary = posts.summary;
-        const shareUrl = 'http://sergiomanzur.com/blog/' + slug;
-        const exampleImage = posts.main_image;
+        ReactGA.initialize('UA-65003463-1');
+        ReactGA.pageview(window.location.pathname + window.location.search);
 
 
-        return (
-            <Container>
-                <Helmet>
-                    <meta charSet="utf-8"/>
-                    <title>{title + " | El Blog de Sergio Manzur"}</title>
-                    <meta name="description" content={summary}/>
-                    <link rel="canonical" href={"http://sergiomanzur.com/blog/" + slug}/>
-                </Helmet>
-                <br/>
-                <Row>
-                    <h2>{title}</h2>
-                    {first_image}
-                    <Col sm={8}>
-                        <Card.Subtitle className={classes.Subtitle}>
-                            {posts.author} - <Moment format="DD/MM/YYYY">{posts.fecha}</Moment>
-                        </Card.Subtitle>
+        if(this.state.posts === false) {
+            return (
+                <NoMatch/>
+            );
+        } else {
 
-                        <div className="Demo__container">
-                            <div className="Demo__some-network">
-                                <FacebookShareButton
-                                    url={shareUrl}
-                                    quote={title}
-                                    className="Demo__some-network__share-button">
-                                    <FacebookIcon
-                                        size={32}
-                                        round />
-                                </FacebookShareButton>
+            const title = this.state.posts.title;
+            const content = [this.state.posts.body];
+            const first_image = <img style={{width: "100%", marginBottom: "25px"}} src={this.state.posts.main_image}/>
+            const summary = this.state.posts.summary;
+            const shareUrl = 'http://sergiomanzur.com/blog/' + this.state.slug;
+            const exampleImage = this.state.posts.main_image;
 
-                                <FacebookShareCount
-                                    url={shareUrl}
-                                    className="Demo__some-network__share-count">
-                                    {count => count}
-                                </FacebookShareCount>
-                            </div>
+            return(
+                <Container>
+                    <Helmet>
+                        <meta charSet="utf-8"/>
+                        <title>{title + " | El Blog de Sergio Manzur"}</title>
+                        <meta name="description" content={summary}/>
+                        <link rel="canonical" href={"http://sergiomanzur.com/blog/" + this.state.slug}/>
+                    </Helmet>
+                    <br/>
+                    <Row>
 
-                            <div className="Demo__some-network">
-                                <TwitterShareButton
-                                    url={shareUrl}
-                                    title={title}
-                                    className="Demo__some-network__share-button">
-                                    <TwitterIcon
-                                        size={32}
-                                        round />
-                                </TwitterShareButton>
+                        <h2>{title}</h2>
+                        {first_image}
+                        <Col sm={8}>
+                            <Card.Subtitle className={classes.Subtitle}>
+                                {this.state.posts.author} - <Moment format="DD/MM/YYYY">{this.state.posts.fecha}</Moment>
+                            </Card.Subtitle>
 
-                                <div className="Demo__some-network__share-count">
-                                    &nbsp;
+                            <div className="Demo__container">
+                                <div className="Demo__some-network">
+                                    <FacebookShareButton
+                                        url={shareUrl}
+                                        quote={title}
+                                        className="Demo__some-network__share-button">
+                                        <FacebookIcon
+                                            size={32}
+                                            round />
+                                    </FacebookShareButton>
+
+                                    <FacebookShareCount
+                                        url={shareUrl}
+                                        className="Demo__some-network__share-count">
+                                        {count => count}
+                                    </FacebookShareCount>
                                 </div>
-                            </div>
 
-                            <div className="Demo__some-network">
-                                <TelegramShareButton
-                                    url={shareUrl}
-                                    title={title}
-                                    className="Demo__some-network__share-button">
-                                    <TelegramIcon size={32} round />
-                                </TelegramShareButton>
+                                <div className="Demo__some-network">
+                                    <TwitterShareButton
+                                        url={shareUrl}
+                                        title={title}
+                                        className="Demo__some-network__share-button">
+                                        <TwitterIcon
+                                            size={32}
+                                            round />
+                                    </TwitterShareButton>
 
-                                <div className="Demo__some-network__share-count">
-                                    &nbsp;
+                                    <div className="Demo__some-network__share-count">
+                                        &nbsp;
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="Demo__some-network">
-                                <WhatsappShareButton
-                                    url={shareUrl}
-                                    title={title}
-                                    separator=":: "
-                                    className="Demo__some-network__share-button">
-                                    <WhatsappIcon size={32} round />
-                                </WhatsappShareButton>
+                                <div className="Demo__some-network">
+                                    <TelegramShareButton
+                                        url={shareUrl}
+                                        title={title}
+                                        className="Demo__some-network__share-button">
+                                        <TelegramIcon size={32} round />
+                                    </TelegramShareButton>
 
-                                <div className="Demo__some-network__share-count">
-                                    &nbsp;
+                                    <div className="Demo__some-network__share-count">
+                                        &nbsp;
+                                    </div>
                                 </div>
+
+                                <div className="Demo__some-network">
+                                    <WhatsappShareButton
+                                        url={shareUrl}
+                                        title={title}
+                                        separator=":: "
+                                        className="Demo__some-network__share-button">
+                                        <WhatsappIcon size={32} round />
+                                    </WhatsappShareButton>
+
+                                    <div className="Demo__some-network__share-count">
+                                        &nbsp;
+                                    </div>
+                                </div>
+
+                                <div className="Demo__some-network">
+                                    <LinkedinShareButton
+                                        url={shareUrl}
+                                        windowWidth={750}
+                                        windowHeight={600}
+                                        className="Demo__some-network__share-button">
+                                        <LinkedinIcon
+                                            size={32}
+                                            round />
+                                    </LinkedinShareButton>
+                                </div>
+
+                                <div className="Demo__some-network">
+                                    <PinterestShareButton
+                                        url={String(window.location)}
+                                        media={`${String(window.location)}/${exampleImage}`}
+                                        windowWidth={1000}
+                                        windowHeight={730}
+                                        className="Demo__some-network__share-button">
+                                        <PinterestIcon size={32} round />
+                                    </PinterestShareButton>
+
+                                    <PinterestShareCount url={shareUrl}
+                                                         className="Demo__some-network__share-count" />
+                                </div>
+
+                                <div className="Demo__some-network">
+                                    <RedditShareButton
+                                        url={shareUrl}
+                                        title={title}
+                                        windowWidth={660}
+                                        windowHeight={460}
+                                        className="Demo__some-network__share-button">
+                                        <RedditIcon
+                                            size={32}
+                                            round />
+                                    </RedditShareButton>
+
+                                    <RedditShareCount url={shareUrl}
+                                                      className="Demo__some-network__share-count" />
+                                </div>
+
+                                <div className="Demo__some-network">
+                                    <EmailShareButton
+                                        url={shareUrl}
+                                        subject={title}
+                                        body="body"
+                                        className="Demo__some-network__share-button">
+                                        <EmailIcon
+                                            size={32}
+                                            round />
+                                    </EmailShareButton>
+                                </div>
+
                             </div>
 
-                            <div className="Demo__some-network">
-                                <LinkedinShareButton
-                                    url={shareUrl}
-                                    windowWidth={750}
-                                    windowHeight={600}
-                                    className="Demo__some-network__share-button">
-                                    <LinkedinIcon
-                                        size={32}
-                                        round />
-                                </LinkedinShareButton>
+
+                            <div className="embed-wrapper" dangerouslySetInnerHTML={{__html: content}}></div>
+                        </Col>
+                        <Col sm={4} style={{padding: "30px"}}>
+                            <div style={{textAlign: 'center'}}>
+                                <Image style={{width: 100}}
+                                       src="https://scontent.fmex4-1.fna.fbcdn.net/v/t1.0-9/22450165_10159513665515302_7682864324370379155_n.jpg?_nc_cat=107&_nc_oc=AQnr82w4pzFJaX8ELmrs7aUGxFr0vE_eyzkqzynEOtaiMVU9KsL9LERKP6hXZ2iYa_M&_nc_ht=scontent.fmex4-1.fna&oh=4ccb0f6d863d0d1b5c4ff6fcab03cc57&oe=5DDD40E6"
+                                       roundedCircle/>
                             </div>
-
-                            <div className="Demo__some-network">
-                                <PinterestShareButton
-                                    url={String(window.location)}
-                                    media={`${String(window.location)}/${exampleImage}`}
-                                    windowWidth={1000}
-                                    windowHeight={730}
-                                    className="Demo__some-network__share-button">
-                                    <PinterestIcon size={32} round />
-                                </PinterestShareButton>
-
-                                <PinterestShareCount url={shareUrl}
-                                                     className="Demo__some-network__share-count" />
-                            </div>
-
-                            <div className="Demo__some-network">
-                                <RedditShareButton
-                                    url={shareUrl}
-                                    title={title}
-                                    windowWidth={660}
-                                    windowHeight={460}
-                                    className="Demo__some-network__share-button">
-                                    <RedditIcon
-                                        size={32}
-                                        round />
-                                </RedditShareButton>
-
-                                <RedditShareCount url={shareUrl}
-                                                  className="Demo__some-network__share-count" />
-                            </div>
-
-                            <div className="Demo__some-network">
-                                <EmailShareButton
-                                    url={shareUrl}
-                                    subject={title}
-                                    body="body"
-                                    className="Demo__some-network__share-button">
-                                    <EmailIcon
-                                        size={32}
-                                        round />
-                                </EmailShareButton>
-                            </div>
-
-                        </div>
-
-
-                        <div className="embed-wrapper" dangerouslySetInnerHTML={{__html: content}}></div>
-                    </Col>
-                    <Col sm={4} style={{padding: "30px"}}>
-                        <div style={{textAlign: 'center'}}>
-                            <Image style={{width: 100}}
-                                   src="https://scontent.fmex4-1.fna.fbcdn.net/v/t1.0-9/22450165_10159513665515302_7682864324370379155_n.jpg?_nc_cat=107&_nc_oc=AQnr82w4pzFJaX8ELmrs7aUGxFr0vE_eyzkqzynEOtaiMVU9KsL9LERKP6hXZ2iYa_M&_nc_ht=scontent.fmex4-1.fna&oh=4ccb0f6d863d0d1b5c4ff6fcab03cc57&oe=5DDD40E6"
-                                   roundedCircle/>
-                        </div>
-                        <br/>
-                        <MostRead/>
-                        <br/>
-                        <Latest/>
-                    </Col>
-                </Row>
-            </Container>
-        )
+                            <br/>
+                            <MostRead/>
+                            <br/>
+                            <Latest/>
+                        </Col>
+                    </Row>
+                </Container>
+            );
+        }
     }
 }
 
