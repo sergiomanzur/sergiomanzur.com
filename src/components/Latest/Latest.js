885c051e951e3d118f12d53a75b5e3f4169fe40e
Sergio Manzur Tapia <sergio@mitunetwork.com>
2020-02-13 16:37:07 -0600

    implement reducer on most read and latest posts sections

--- src/components/Latest/Latest.js
+++ src/components/Latest/Latest.js
@@ -1,26 +1,58 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useReducer} from 'react';
 import classes from './Latest.module.css';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 import axios from 'axios';
+import Spinner from "react-bootstrap/Spinner";
 
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'data_received':
+            return {
+                ...state,
+                posts: action.payload,
+                isLoading: false,
+                isError: false
+            };
+        case 'getting_data':
+            return {
+                ...state,
+                isLoading: true,
+                isError: false
+            };
+        case 'getting_data_error' :
+            return {
+                ...state,
+                isError: true,
+                isLoading: false,
+            };
+
+        default:
+            throw new Error();
+    }
+}
 
 const Latest = () => {
 
-    const [posts,setPost] = useState([]);
+    const [state, dispatch] = useReducer(reducer,{
+        posts: [],
+        isLoading: false,
+        isError: false
+    });
 
     useEffect(() => {
-
+        dispatch({type:'getting_data'});
         axios.get('http://www.dev.sergiomanzur.com/api/pages/')
             .then(res => {
-                setPost(res.data.data);
+                dispatch({type:'data_received',payload: res.data.data});
             })
     }, []);
 
 
-    const latestPosts = posts.filter((x,i)=> i < 5 ).map(
+    const latestPosts = state.posts.filter((x,i)=> i < 5 ).map(
         x =>
             <Row key={x.id} className={classes.latestRow}>
             <Col sm={4}>
@@ -32,11 +64,21 @@ const Latest = () => {
         </Row>
     );
 
+    const loadingMessage = state.isLoading ? (
+        <Spinner style={{flex: 1, alignSelf: 'center' }} animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+        </Spinner>
+    ) : null;
+
+    const error = state.isError ? (console.log("error loading most read.")) : null;
+
     return (
         <Container className={classes.latest}>
             <Row style={{display:"flex", justifyContent:"center", alignItems: "center"}}><strong>Más Nuevos</strong></Row>
             <br/>
             {latestPosts}
+            {loadingMessage}
+            {error}
         </Container>
     )
 }
--- src/components/MostRead/MostRead.js
+++ src/components/MostRead/MostRead.js
@@ -1,26 +1,62 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useReducer} from 'react';
 import classes from './MostRead.module.css';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
+import Spinner from "react-bootstrap/Spinner";
 import axios from 'axios';
+
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'data_received':
+            return {
+              ...state,
+              posts: action.payload,
+              isLoading: false,
+              isError: false
+            };
+        case 'getting_data':
+            return {
+                ...state,
+                isLoading: true,
+                isError: false
+            };
+        case 'getting_data_error' :
+            return {
+                ...state,
+                isError: true,
+                isLoading: false,
+            };
+
+        default:
+            throw new Error();
+    }
+}
 
 
 const MostRead = () => {
 
-    const [posts,setPost] = useState([]);
+    const [state, dispatch] = useReducer(reducer,{
+        posts: [],
+        isLoading: false,
+        isError: false
+    });
+
 
     useEffect(() => {
-
+        dispatch({type:'getting_data'});
         axios.get('http://www.dev.sergiomanzur.com/api/most-read/')
             .then(res => {
-                setPost(res.data.data);
-            })
+                dispatch({type:'data_received',payload: res.data.data});
+            }). catch( () => {
+                dispatch({type:'getting_data_error'});
+        })
     }, []);
 
 
-    const mostReadPosts = posts.filter((x,i)=> i < 5 ).map(
+    const mostReadPosts = state.posts.filter((x,i)=> i < 5 ).map(
         x =>
             <Row key={x.id} className={classes.mostReadRow}>
             <Col sm={4}>
@@ -32,13 +68,24 @@ const MostRead = () => {
         </Row>
     );
 
+    const loadingMessage = state.isLoading ? (
+        <Spinner style={{flex: 1, alignSelf: 'center' }} animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+        </Spinner>
+    ) : null;
+
+    const error = state.isError ? (console.log("error loading most read.")) : null;
+
     return (
         <Container className={classes.mostRead}>
             <Row style={{display:"flex", justifyContent:"center", alignItems: "center"}}><strong>Más Populares</strong></Row>
             <br/>
+            {loadingMessage}
             {mostReadPosts}
+            {error}
         </Container>
     )
 }
 
 export default MostRead;
+
